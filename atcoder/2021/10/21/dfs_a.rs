56db5ff3856328ce53ca20e23bce3256188b6e62
katand <katandps@gmail.com>
2021-10-22 01:42:05 +0900

    practice2021/10/21

---
+++ atcoder/2021/10/21/dfs_a.rs
@@ -0,0 +1,471 @@
+/// general import
+pub use std::{
+    cmp::{max, min, Ordering, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    convert::Infallible,
+    convert::{TryFrom, TryInto},
+    fmt::{Debug, Display, Formatter},
+    io::{stdin, stdout, BufRead, BufWriter, Write},
+    iter::{Product, Sum},
+    marker::PhantomData,
+    mem::swap,
+    ops::{
+        Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Bound,
+        Deref, DerefMut, Div, DivAssign, Mul, MulAssign, Neg, Not, RangeBounds, Rem, RemAssign,
+        Shl, ShlAssign, Shr, ShrAssign, Sub, SubAssign,
+    },
+    str::{from_utf8, FromStr},
+};
+
+/// min-max macros
+#[allow(unused_macros)]
+macro_rules! chmin {($base:expr, $($cmps:expr),+ $(,)*) => {{let cmp_min = min!($($cmps),+);if $base > cmp_min {$base = cmp_min;true} else {false}}};}
+#[allow(unused_macros)]
+macro_rules! chmax {($base:expr, $($cmps:expr),+ $(,)*) => {{let cmp_max = max!($($cmps),+);if $base < cmp_max {$base = cmp_max;true} else {false}}};}
+#[allow(unused_macros)]
+macro_rules! min {
+    ($a:expr $(,)*) => {{$a}};
+    ($a:expr, $b:expr $(,)*) => {{if $a > $b {$b} else {$a}}};
+    ($a:expr, $($rest:expr),+ $(,)*) => {{let b = min!($($rest),+);if $a > b {b} else {$a}}};
+}
+#[allow(unused_macros)]
+macro_rules! max {
+    ($a:expr $(,)*) => {{$a}};
+    ($a:expr, $b:expr $(,)*) => {{if $a > $b {$a} else {$b}}};
+    ($a:expr, $($rest:expr),+ $(,)*) => {{let b = max!($($rest),+);if $a > b {$a} else {b}}};
+}
+
+/// stdin reader
+pub struct Reader<R: BufRead> {
+    reader: R,
+    buf: Vec<u8>,
+    pos: usize,
+}
+
+macro_rules! prim_method {
+    ($name:ident: $T: ty) => {
+        pub fn $name(&mut self) -> $T {
+            self.n::<$T>()
+        }
+    };
+    ($name:ident) => {
+        prim_method!($name: $name);
+    }
+}
+macro_rules! prim_methods {
+    ($name:ident: $T:ty; $($rest:tt)*) => {
+        prim_method!($name:$T);
+        prim_methods!($($rest)*);
+    };
+    ($name:ident; $($rest:tt)*) => {
+        prim_method!($name);
+        prim_methods!($($rest)*);
+    };
+    () => ()
+}
+
+macro_rules! replace_expr {
+    ($_t:tt $sub:expr) => {
+        $sub
+    };
+}
+macro_rules! tuple_method {
+    ($name: ident: ($($T:ident),+)) => {
+        pub fn $name(&mut self) -> ($($T),+) {
+            ($(replace_expr!($T self.n())),+)
+        }
+    }
+}
+macro_rules! tuple_methods {
+    ($name:ident: ($($T:ident),+); $($rest:tt)*) => {
+        tuple_method!($name:($($T),+));
+        tuple_methods!($($rest)*);
+    };
+    () => ()
+}
+macro_rules! vec_method {
+    ($name: ident: ($($T:ty),+)) => {
+        pub fn $name(&mut self, n: usize) -> Vec<($($T),+)> {
+            (0..n).map(|_|($(replace_expr!($T self.n())),+)).collect()
+        }
+    };
+    ($name: ident: $T:ty) => {
+        pub fn $name(&mut self, n: usize) -> Vec<$T> {
+            (0..n).map(|_|self.n()).collect()
+        }
+    };
+}
+macro_rules! vec_methods {
+    ($name:ident: ($($T:ty),+); $($rest:tt)*) => {
+        vec_method!($name:($($T),+));
+        vec_methods!($($rest)*);
+    };
+    ($name:ident: $T:ty; $($rest:tt)*) => {
+        vec_method!($name:$T);
+        vec_methods!($($rest)*);
+    };
+    () => ()
+}
+impl<R: BufRead> Reader<R> {
+    pub fn new(reader: R) -> Reader<R> {
+        let (buf, pos) = (Vec::new(), 0);
+        Reader { reader, buf, pos }
+    }
+    prim_methods! {
+        u: usize; i: i64; f: f64; str: String; c: char; string: String;
+        u8; u16; u32; u64; u128; usize; i8; i16; i32; i64; i128; isize; f32; f64; char;
+    }
+    tuple_methods! {
+        u2: (usize, usize); u3: (usize, usize, usize); u4: (usize, usize, usize, usize);
+        i2: (i64, i64); i3: (i64, i64, i64); i4: (i64, i64, i64, i64);
+        cuu: (char, usize, usize);
+    }
+    vec_methods! {
+        uv: usize; uv2: (usize, usize); uv3: (usize, usize, usize);
+        iv: i64; iv2: (i64, i64); iv3: (i64, i64, i64);
+        vq: (char, usize, usize);
+    }
+
+    pub fn n<T: FromStr>(&mut self) -> T
+        where
+            T::Err: Debug,
+    {
+        self.n_op().unwrap()
+    }
+
+    pub fn n_op<T: FromStr>(&mut self) -> Option<T>
+        where
+            T::Err: Debug,
+    {
+        if self.buf.is_empty() {
+            self._read_next_line();
+        }
+        let mut start = None;
+        while self.pos != self.buf.len() {
+            match (self.buf[self.pos], start.is_some()) {
+                (b' ', true) | (b'\n', true) => break,
+                (_, true) | (b' ', false) => self.pos += 1,
+                (b'\n', false) => self._read_next_line(),
+                (_, false) => start = Some(self.pos),
+            }
+        }
+        start.map(|s| from_utf8(&self.buf[s..self.pos]).unwrap().parse().unwrap())
+    }
+
+    fn _read_next_line(&mut self) {
+        self.pos = 0;
+        self.buf.clear();
+        self.reader.read_until(b'\n', &mut self.buf).unwrap();
+    }
+    pub fn s(&mut self) -> Vec<char> {
+        self.n::<String>().chars().collect()
+    }
+    pub fn digits(&mut self) -> Vec<i64> {
+        self.n::<String>()
+            .chars()
+            .map(|c| (c as u8 - b'0') as i64)
+            .collect()
+    }
+    pub fn char_map(&mut self, h: usize) -> Vec<Vec<char>> {
+        (0..h).map(|_| self.s()).collect()
+    }
+    /// charの2次元配列からboolのmapを作る ngで指定した壁のみfalseとなる
+    pub fn bool_map(&mut self, h: usize, ng: char) -> Vec<Vec<bool>> {
+        self.char_map(h)
+            .iter()
+            .map(|v| v.iter().map(|&c| c != ng).collect())
+            .collect()
+    }
+    /// h*w行列を取得する
+    pub fn matrix(&mut self, h: usize, w: usize) -> Vec<Vec<i64>> {
+        (0..h).map(|_| self.iv(w)).collect()
+    }
+}
+
+/// stdin writer
+pub struct Writer<W: Write> {
+    w: BufWriter<W>,
+}
+impl<W: Write> Writer<W> {
+    pub fn new(writer: W) -> Writer<W> {
+        Writer {
+            w: BufWriter::new(writer),
+        }
+    }
+
+    pub fn println<S: Display>(&mut self, s: &S) {
+        writeln!(self.w, "{}", s).unwrap()
+    }
+
+    pub fn print<S: Display>(&mut self, s: &S) {
+        write!(self.w, "{}", s).unwrap()
+    }
+
+    pub fn print_join<S: Display>(&mut self, v: &[S], separator: Option<&str>) {
+        let sep = separator.unwrap_or_else(|| "\n");
+        writeln!(
+            self.w,
+            "{}",
+            v.iter()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+                .join(sep)
+        )
+            .unwrap()
+    }
+}
+
+#[derive(Debug)]
+pub struct Grid<W> {
+    pub h: usize,
+    pub w: usize,
+    pub size: usize,
+    pub map: Vec<W>,
+}
+
+impl<W: Clone> GraphTrait for Grid<W> {
+    type Weight = W;
+
+    fn size(&self) -> usize {
+        self.size
+    }
+
+    fn edges(&self, src: usize) -> Vec<Edge<W>> {
+        let mut ret = Vec::new();
+        if self.x(src) + 1 < self.w {
+            ret.push(Edge::new(src, src + 1, self.map[src + 1].clone()));
+        }
+        if self.y(src) + 1 < self.h {
+            ret.push(Edge::new(src, src + self.w, self.map[src + self.w].clone()));
+        }
+        if self.x(src) > 0 {
+            ret.push(Edge::new(src, src - 1, self.map[src - 1].clone()));
+        }
+        if self.y(src) > 0 {
+            ret.push(Edge::new(src, src - self.w, self.map[src - self.w].clone()));
+        }
+        ret
+    }
+
+    fn rev_edges(&self, dst: usize) -> Vec<Edge<W>> {
+        let mut ret = Vec::new();
+        if self.x(dst) + 1 < self.w {
+            ret.push(Edge::new(dst, dst + 1, self.map[dst].clone()));
+        }
+        if self.y(dst) + 1 < self.h {
+            ret.push(Edge::new(dst, dst + self.w, self.map[dst].clone()));
+        }
+        if self.x(dst) > 0 {
+            ret.push(Edge::new(dst, dst - 1, self.map[dst].clone()));
+        }
+        if self.y(dst) > 0 {
+            ret.push(Edge::new(dst, dst - self.w, self.map[dst].clone()));
+        }
+        ret
+    }
+}
+
+impl<W: Clone> Grid<W> {
+    pub fn new(h: usize, w: usize, input: Vec<Vec<W>>) -> Grid<W> {
+        let mut map = Vec::new();
+        for r in input {
+            for c in r {
+                map.push(c);
+            }
+        }
+        let max = h * w;
+        Grid {
+            h,
+            w,
+            size: max,
+            map,
+        }
+    }
+    pub fn key(&self, x: usize, y: usize) -> usize {
+        y * self.w + x
+    }
+    pub fn xy(&self, k: usize) -> (usize, usize) {
+        (self.x(k), self.y(k))
+    }
+    pub fn x(&self, k: usize) -> usize {
+        k % self.w
+    }
+    pub fn y(&self, k: usize) -> usize {
+        k / self.w
+    }
+    pub fn get(&self, key: usize) -> &W {
+        &self.map[key]
+    }
+    pub fn set(&mut self, key: usize, value: W) {
+        self.map[key] = value;
+    }
+}
+
+#[derive(Copy, Clone, Eq, Default)]
+pub struct Edge<W> {
+    pub src: usize,
+    pub dst: usize,
+    pub weight: W,
+}
+
+impl<W: Display> Debug for Edge<W> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} -> {} : {}", self.src, self.dst, self.weight)
+    }
+}
+
+impl<W> Edge<W> {
+    pub fn new(src: usize, dst: usize, weight: W) -> Self {
+        Edge { src, dst, weight }
+    }
+}
+
+impl<W: PartialEq> PartialEq for Edge<W> {
+    fn eq(&self, other: &Self) -> bool {
+        self.weight.eq(&other.weight)
+    }
+}
+
+impl<W: PartialOrd> PartialOrd for Edge<W> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.weight.partial_cmp(&other.weight)
+    }
+}
+
+impl<W: PartialOrd + Eq> Ord for Edge<W> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.weight.partial_cmp(&other.weight).expect("Found NAN")
+    }
+}
+
+pub trait GraphTrait {
+    type Weight;
+    fn size(&self) -> usize;
+    fn edges(&self, src: usize) -> Vec<Edge<Self::Weight>>;
+    fn rev_edges(&self, dst: usize) -> Vec<Edge<Self::Weight>>;
+    /// 各頂点の入次数を返す
+    fn indegree(&self) -> Vec<i32> {
+        (0..self.size())
+            .map(|dst| self.rev_edges(dst).len() as i32)
+            .collect()
+    }
+
+    /// 各頂点の出次数を返す
+    fn outdegree(&self) -> Vec<i32> {
+        (0..self.size())
+            .map(|src| self.edges(src).len() as i32)
+            .collect()
+    }
+}
+
+impl<W: Clone> GraphTrait for Graph<W> {
+    type Weight = W;
+    fn size(&self) -> usize {
+        self.n
+    }
+    fn edges(&self, src: usize) -> Vec<Edge<W>> {
+        self.edges[src].clone()
+    }
+    fn rev_edges(&self, src: usize) -> Vec<Edge<W>> {
+        self.rev_edges[src].clone()
+    }
+}
+
+/// 辺の情報を使用してグラフの問題を解くためのライブラリ
+pub struct Graph<W> {
+    pub n: usize,
+    pub edges: Vec<Vec<Edge<W>>>,
+    pub rev_edges: Vec<Vec<Edge<W>>>,
+}
+
+impl<W: Clone> Clone for Graph<W> {
+    fn clone(&self) -> Self {
+        Self {
+            n: self.n,
+            edges: self.edges.clone(),
+            rev_edges: self.rev_edges.clone(),
+        }
+    }
+}
+
+/// i64の隣接行列からグラフを生成する O(N^2)
+impl From<&Vec<Vec<i64>>> for Graph<i64> {
+    fn from(w: &Vec<Vec<i64>>) -> Self {
+        let n = w.len();
+        let mut ret = Self::new(n);
+        for i in 0..n {
+            assert_eq!(n, w[i].len());
+            for j in i + 1..n {
+                if w[i][j] == -1 {
+                    continue;
+                }
+                ret.add_edge(i, j, w[i as usize][j as usize]);
+                ret.add_edge(j, i, w[j as usize][i as usize]);
+            }
+        }
+        ret
+    }
+}
+
+impl<W: Clone> Graph<W> {
+    /// n: 頂点数
+    pub fn new(n: usize) -> Self {
+        Self {
+            n,
+            edges: vec![Vec::new(); n],
+            rev_edges: vec![Vec::new(); n],
+        }
+    }
+    /// 相互に行き来できる辺をつける
+    pub fn add_edge(&mut self, a: usize, b: usize, w: W) {
+        self.edges[a].push(Edge::new(a, b, w.clone()));
+        self.edges[b].push(Edge::new(b, a, w.clone()));
+        self.rev_edges[a].push(Edge::new(a, b, w.clone()));
+        self.rev_edges[b].push(Edge::new(b, a, w));
+    }
+
+    /// 1方向にのみ移動できる辺をつける
+    pub fn add_arc(&mut self, a: usize, b: usize, w: W) {
+        self.edges[a].push(Edge::new(a, b, w.clone()));
+        self.rev_edges[b].push(Edge::new(b, a, w));
+    }
+}
+
+#[allow(dead_code)]
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    solve(Reader::new(stdin.lock()), Writer::new(stdout.lock()));
+}
+
+pub fn solve<R: BufRead, W: Write>(mut reader: Reader<R>, mut writer: Writer<W>) {
+    let (h, w) = reader.u2();
+    let c = reader.char_map(h);
+    let grid = Grid::new(h, w, c);
+
+    let mut start = 0;
+    let mut goal = 0;
+    for i in 0..h * w {
+        if grid.get(i) == &'s' {
+            start = i;
+        }
+        if grid.get(i) == &'g' {
+            goal = i;
+        }
+    }
+
+    let mut q = VecDeque::new();
+    q.push_back(start);
+    let mut dist = vec![false; h * w];
+    dist[start] = true;
+    while let Some(src) = q.pop_front() {
+        for edge in grid.edges(src) {
+            if dist[edge.dst] || grid.get(edge.dst) == &'#' {
+                continue;
+            }
+            dist[edge.dst] = true;
+            q.push_back(edge.dst);
+        }
+    }
+    writer.println(&if dist[goal] { "Yes" } else { "No" });
+}
