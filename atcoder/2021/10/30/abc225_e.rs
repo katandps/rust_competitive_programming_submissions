38b21dc6ee10f8ad798a0a525a53fd925a3de714
katand <katandps@gmail.com>
2021-10-31 00:59:09 +0900

    practice2021/10/30

---
+++ atcoder/2021/10/30/abc225_e.rs
@@ -0,0 +1,186 @@
+/// general import
+pub use std::{
+    cmp::{max, min, Ordering, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    convert::Infallible,
+    convert::{TryFrom, TryInto},
+    fmt::{Debug, Display, Formatter},
+    io::{stdin, stdout, BufRead, BufWriter, Write},
+    iter::{Product, Sum},
+    marker::PhantomData,
+    mem::swap,
+    ops::{
+        Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Bound,
+        Deref, DerefMut, Div, DivAssign, Mul, MulAssign, Neg, Not, RangeBounds, Rem, RemAssign,
+        Shl, ShlAssign, Shr, ShrAssign, Sub, SubAssign,
+    },
+    str::{from_utf8, FromStr},
+};
+
+/// min-max macros
+#[allow(unused_macros)]
+macro_rules! chmin {($base:expr, $($cmps:expr),+ $(,)*) => {{let cmp_min = min!($($cmps),+);if $base > cmp_min {$base = cmp_min;true} else {false}}};}
+#[allow(unused_macros)]
+macro_rules! chmax {($base:expr, $($cmps:expr),+ $(,)*) => {{let cmp_max = max!($($cmps),+);if $base < cmp_max {$base = cmp_max;true} else {false}}};}
+#[allow(unused_macros)]
+macro_rules! min {($a:expr $(,)*) => {{$a}};($a:expr, $b:expr $(,)*) => {{if $a > $b {$b} else {$a}}};($a:expr, $($rest:expr),+ $(,)*) => {{let b = min!($($rest),+);if $a > b {b} else {$a}}};}
+#[allow(unused_macros)]
+macro_rules! max {($a:expr $(,)*) => {{$a}};($a:expr, $b:expr $(,)*) => {{if $a > $b {$a} else {$b}}};($a:expr, $($rest:expr),+ $(,)*) => {{let b = max!($($rest),+);if $a > b {$a} else {b}}};}
+
+pub fn to_lr<R: RangeBounds<usize>>(range: R, length: usize) -> (usize, usize) {
+    let l = match range.start_bound() {
+        Bound::Unbounded => 0,
+        Bound::Included(&s) => s,
+        Bound::Excluded(&s) => s + 1,
+    };
+    let r = match range.end_bound() {
+        Bound::Unbounded => length,
+        Bound::Included(&e) => e + 1,
+        Bound::Excluded(&e) => e,
+    };
+    assert!(l <= r && r <= length);
+    (l, r)
+}
+
+/// stdin reader
+pub struct Reader<R> {
+    reader: R,
+    buf: VecDeque<String>,
+}
+impl<R: BufRead> Iterator for Reader<R> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        if self.buf.is_empty() {
+            let mut buf = Vec::new();
+            self.reader.read_to_end(&mut buf).unwrap();
+            let s = from_utf8(&buf).expect("utf8でない文字列が入力されました.");
+            s.split_whitespace()
+                .map(ToString::to_string)
+                .for_each(|s| self.buf.push_back(s));
+        }
+        self.buf.pop_front()
+    }
+}
+impl<R: BufRead> Reader<R> {
+    pub fn new(reader: R) -> Reader<R> {
+        Reader {
+            reader,
+            buf: VecDeque::new(),
+        }
+    }
+    pub fn val<T: FromStr>(&mut self) -> T {
+        self.next()
+            .map(|token| token.parse().ok().expect("型変換エラー"))
+            .expect("入力が足りません")
+    }
+    pub fn vec<T: FromStr>(&mut self, length: usize) -> Vec<T> {
+        (0..length).map(|_| self.val()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.val::<String>().chars().collect()
+    }
+    pub fn digits(&mut self) -> Vec<i64> {
+        self.val::<String>()
+            .chars()
+            .map(|c| (c as u8 - b'0') as i64)
+            .collect()
+    }
+    pub fn char_map(&mut self, h: usize) -> Vec<Vec<char>> {
+        (0..h).map(|_| self.chars()).collect()
+    }
+    pub fn bool_map(&mut self, h: usize, ng: char) -> Vec<Vec<bool>> {
+        self.char_map(h)
+            .iter()
+            .map(|v| v.iter().map(|&c| c != ng).collect())
+            .collect()
+    }
+    pub fn matrix<T: FromStr>(&mut self, h: usize, w: usize) -> Vec<Vec<T>> {
+        (0..h).map(|_| self.vec(w)).collect()
+    }
+}
+
+/// stdin writer
+pub struct Writer<W: Write> {
+    writer: BufWriter<W>,
+}
+impl<W: Write> Writer<W> {
+    pub fn new(write: W) -> Self {
+        Self {
+            writer: BufWriter::new(write),
+        }
+    }
+    pub fn println<S: Display>(&mut self, s: S) {
+        writeln!(self.writer, "{}", s).expect("Failed to write.")
+    }
+    pub fn print<S: Display>(&mut self, s: S) {
+        write!(self.writer, "{}", s).expect("Failed to write.")
+    }
+    pub fn print_join<S: Display>(&mut self, v: &[S], separator: &str) {
+        v.iter().fold("", |sep, arg| {
+            write!(self.writer, "{}{}", sep, arg).expect("Failed to write.");
+            separator
+        });
+        writeln!(self.writer).expect("Failed to write.");
+    }
+}
+
+#[allow(dead_code)]
+fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+    solve(Reader::new(stdin.lock()), Writer::new(stdout.lock()));
+}
+
+pub fn solve<R: BufRead, W: Write>(mut reader: Reader<R>, mut writer: Writer<W>) {
+    let n = reader.val::<usize>();
+    let xy = (0..n)
+        .map(|_| (reader.val(), reader.val()))
+        .collect::<Vec<(i64, i64)>>();
+    let mut p = Vec::new();
+    for i in 0..n {
+        let (x, y) = xy[i];
+        p.push(((x - 1, y), i));
+        p.push(((x, y - 1), i));
+    }
+    p.sort_by(|a, b| {
+        ((a.0).1 * (b.0).0)
+            .partial_cmp(&((a.0).0 * (b.0).1))
+            .unwrap()
+    });
+    let mut t = 1;
+    let mut d = Vec::new();
+    d.push((1, p[0].1));
+    for i in 1..p.len() {
+        let (a, b) = (p[i], p[i - 1]);
+        if ((a.0).1 * (b.0).0) > ((a.0).0 * (b.0).1) {
+            t += 1;
+        }
+        d.push((t, p[i].1));
+    }
+    let mut count = vec![0; n];
+    let mut l = vec![0; n];
+    let mut r = vec![0; n];
+    for (d, p) in d {
+        if count[p] == 0 {
+            l[p] = d;
+        } else if count[p] == 1 {
+            r[p] = d;
+        }
+        count[p] += 1;
+    }
+    let mut lr = Vec::new();
+    for i in 0..n {
+        lr.push((l[i], r[i]));
+    }
+    lr.sort_by_key(|&(l, r)| (r, l));
+    let mut r = 0;
+    let mut ans = 0;
+    for (li, ri) in lr {
+        if li >= r {
+            ans += 1;
+            chmax!(r, ri);
+        }
+    }
+    writer.println(ans);
+}
